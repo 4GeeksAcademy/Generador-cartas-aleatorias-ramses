b622f7c3db8ec2b14f2d0bf91251803e6791ca3f
ramsesfg <zad.yadiel12@gmail.com>
2025-06-15 23:34:50 +0000

    cartas al azar

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let numbers = [2, 3, 4, 5 , 6, 7, 8, 9, 10, "J", "Q", "K", "A",];
+  let icons = [ "♦", "♥", "♠", "♣"];
+
+  let randomNumb = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomIcons = icons[Math.floor(Math.random() * icons.length)];
+
+
+  let card = document.querySelector("#center")
+  let domIcon1 = document.querySelector("#picaUp")
+  let domIcon2 = document.querySelector("#picaDown")
+
+ if (randomIcons === "♥" || randomIcons === "♦"){
+  domIcon1.style.color = "red";
+  domIcon2.style.color = "red";
+ }
+
+ card.innerText = randomNumb;
+ domIcon1.innerText = randomIcons;
+ domIcon2.innerText = randomIcons;
+  
+}
